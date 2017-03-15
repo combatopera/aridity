d0d1254d6603d700f4f873104ffa866044c9deb1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-15 15:21:39 +0000

    ren

--- eval.py
+++ eval.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from resolve import resolve
+from resolve import resolvetemplate
 import sys
 
 def main():
-    expr, = sys.argv[1:]
-    resolve(expr)
+    template, = sys.argv[1:]
+    resolvetemplate(template)
 
 if '__main__' == __name__:
     main()
--- resolve.py
+++ resolve.py
@@ -4,7 +4,7 @@ import sys, collections
 from aridity.grammar import Text, templateparser, Concat, Number, List
 from aridity.context import Context
 
-def resolve(expr):
+def resolvetemplate(template):
     context = Context()
     builtinsname = '__builtins__'
     scope = {builtinsname: eval(builtinsname)}
@@ -12,12 +12,12 @@ def resolve(expr):
         scope[t.__name__] = t
     for name, obj in eval(sys.stdin.read(), scope).items():
         context[name] = obj
-    sys.stdout.write(Concat(templateparser(expr)).resolve(context, None).cat())
+    sys.stdout.write(Concat(templateparser(template)).resolve(context, None).cat())
 
 def main():
     path, = sys.argv[1:]
     with open(path) as f:
-        resolve(f.read())
+        resolvetemplate(f.read())
 
 if '__main__' == __name__:
     main()
