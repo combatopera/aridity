b2fd5e77a649a0691760a77660f0bfcae64dbb84
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-18 09:03:15 +0000

    eol boundary no longer applies inside a function call

--- aridity/grammar.py
+++ aridity/grammar.py
@@ -196,7 +196,7 @@ class Parser:
         def gettext(pa, boundarychars):
             return Regex(r"[^$\s%s]+" % re.escape(boundarychars)).leaveWhitespace().setParseAction(pa)
         action = Forward()
-        def getoptblank(pa):
+        def getoptblank(pa, boundarychars):
             return Optional(Regex(r"[^\S%s]+" % re.escape(boundarychars)).leaveWhitespace().setParseAction(pa))
         def clauses():
             for o, c in '()', '[]':
@@ -205,14 +205,14 @@ class Parser:
                 def getarg(scalarpa):
                     return (OneOrMore(optargtext + action) + optargtext | gettext(scalarpa, c)).setParseAction(Concat.pa)
                 def getbrackets(blankpa, scalarpa):
-                    optblank = getoptblank(blankpa)
+                    optblank = getoptblank(blankpa, '')
                     return Suppress(o) + ZeroOrMore(optblank + getarg(scalarpa)) + optblank + Suppress(c)
                 yield Suppress('pass') + getbrackets(Text.pa, Text.pa)
                 yield (cls.identifier + getbrackets(Blank.pa, AnyScalar.pa)).setParseAction(Call.pa)
         action << Suppress('$').leaveWhitespace() + Or(clauses()).leaveWhitespace()
         opttext = Optional(gettext(Text.pa, boundarychars))
         arg = OneOrMore(opttext + action) + opttext | gettext(scalarpa, boundarychars)
-        optblank = getoptblank(Blank.pa)
+        optblank = getoptblank(Blank.pa, boundarychars)
         return (ZeroOrMore(optblank + arg) + optblank).parseWithTabs()
 
     def __init__(self, g):
