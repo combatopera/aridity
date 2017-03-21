87833070b6b72f462f1bf08d9917e06ffb1f991a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-21 21:20:23 +0000

    main now returns stdout

--- aridity.py
+++ aridity.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
-from aridityimpl.grammar import loader
+import sys, io
+from aridityimpl.grammar import loader, WriteAndFlush
 from aridityimpl.context import Context
 
 def main(script):
+    stdout = io.StringIO()
     context = Context()
+    context['stdout'] = WriteAndFlush(stdout)
     for entry in loader(script):
         entry.execute(context)
+    return stdout.getvalue()
 
 if '__main__' == __name__:
-    main(sys.stdin.read())
+    sys.stdout.write(main(sys.stdin.read()))
