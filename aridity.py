b32ed21509b71123b8128d7d48adb6bb567593ca
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-23 13:36:47 +0000

    crude but effective support for multi-line commands

--- aridity.py
+++ aridity.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
-import sys, traceback
+import sys, traceback, pyparsing
 from aridityimpl.parser import commandparser
 from aridityimpl.grammar import Stream
 from aridityimpl.context import Context
 from aridityimpl.directives import execute, UnsupportedEntryException
 
+class DanglingStackException(Exception): pass
+
 def repl(instream, outstream):
     context = Context()
     context['stdout'] = Stream(outstream)
+    stack = []
     for line in instream:
-        command = commandparser(line) # TODO: Support multi-line commands.
+        try:
+            command = commandparser(''.join(stack + [line]))
+            stack.clear()
+        except pyparsing.ParseException:
+            stack.append(line)
+            continue
         try:
             execute(command, context)
         except UnsupportedEntryException:
             traceback.print_exc(0)
+    if stack:
+        raise DanglingStackException(stack)
 
 if '__main__' == __name__:
     repl(sys.stdin, sys.stdout)
