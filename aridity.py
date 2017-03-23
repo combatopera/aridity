823e18ef3d72b16874c09c4aa04c73d6412826ac
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-23 13:59:20 +0000

    when non-interactive, blow up on first error

--- aridity.py
+++ aridity.py
@@ -3,12 +3,12 @@
 import sys, traceback, pyparsing
 from aridityimpl.parser import commandparser
 from aridityimpl.grammar import Stream
-from aridityimpl.context import Context
+from aridityimpl.context import Context, NoSuchPathException
 from aridityimpl.directives import execute, UnsupportedEntryException
 
 class DanglingStackException(Exception): pass
 
-def repl(instream, outstream):
+def repl(instream, outstream, interactive = False):
     context = Context()
     context['stdout'] = Stream(outstream)
     stack = []
@@ -21,10 +21,12 @@ def repl(instream, outstream):
             continue
         try:
             execute(command, context)
-        except UnsupportedEntryException:
+        except (UnsupportedEntryException, NoSuchPathException):
+            if not interactive:
+                raise
             traceback.print_exc(0)
     if stack:
         raise DanglingStackException(stack)
 
 if '__main__' == __name__:
-    repl(sys.stdin, sys.stdout)
+    repl(sys.stdin, sys.stdout, True)
