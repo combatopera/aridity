fd64a0ac0dd3653cafd98638e9f4d467eee34228
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-23 13:33:14 +0000

    continue after failed command

--- aridity.py
+++ aridity.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, traceback
 from aridityimpl.parser import commandparser
 from aridityimpl.grammar import Stream
 from aridityimpl.context import Context
-from aridityimpl.directives import execute
+from aridityimpl.directives import execute, UnsupportedEntryException
 
 def repl(instream, outstream):
     context = Context()
     context['stdout'] = Stream(outstream)
     for line in instream:
         command = commandparser(line) # TODO: Support multi-line commands.
-        execute(command, context)
+        try:
+            execute(command, context)
+        except UnsupportedEntryException:
+            traceback.print_exc(0)
 
 if '__main__' == __name__:
     repl(sys.stdin, sys.stdout)
