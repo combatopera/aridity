9fe7cb11f8f2fc2a14eb6f42b26b95c9afa85d07
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-03-26 19:07:15 +0100

    pyven

--- aridity.py
+++ aridity.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# Copyright 2017 Andrzej Cichocki
+
+# This file is part of aridity.
+#
+# aridity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# aridity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with aridity.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys, traceback, pyparsing
 from aridityimpl.parser import commandparser
 from aridityimpl.grammar import Stream
@@ -28,5 +45,8 @@ def repl(instream, outstream, interactive = False):
     if stack:
         raise DanglingStackException(stack)
 
-if '__main__' == __name__:
+def main():
     repl(sys.stdin, sys.stdout, True)
+
+if '__main__' == __name__:
+    main()
--- aridityimpl/parser.py
+++ aridityimpl/parser.py
@@ -1,3 +1,20 @@
+# Copyright 2017 Andrzej Cichocki
+
+# This file is part of aridity.
+#
+# aridity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# aridity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with aridity.  If not, see <http://www.gnu.org/licenses/>.
+
 from pyparsing import Forward, OneOrMore, Optional, Or, Regex, Suppress, ZeroOrMore, CharsNotIn, NoMatch
 from decimal import Decimal
 from .grammar import Text, Boolean, Number, Blank, Call, Concat, Boundary, Entry
