6bee415cb40014be0f59d0f48c0bba4b3a83d314
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-04-14 11:31:25 +0100

    improve design so i can use this in minimake

--- aridity.py
+++ aridity.py
@@ -23,48 +23,55 @@ from aridityimpl.grammar import Stream
 from aridityimpl.context import Context, NoSuchPathException
 from aridityimpl.directives import execute, UnsupportedEntryException
 
-class Script:
+class DanglingStackException(Exception): pass
+
+class Repl:
 
     u = '[$\r\n]'
     pattern = re.compile(r"^\s+%s*|%s*\s+$|%s+" % (u, u, u))
     del u
 
-    def __init__(self):
-        self.lines = []
-
-    def quote(self, text):
-        return self.pattern.sub(lambda m: "$lit(%s)" % m.group(), text)
-
-    def __call__(self, template, *args):
-        self.lines.append(template % tuple(self.quote(a) for a in args))
-
-    def __iter__(self):
-        return iter(self.lines)
-
-class DanglingStackException(Exception): pass
-
-def repl(instream, outstream, interactive = False):
-    context = Context()
-    context['stdout'] = Stream(outstream)
-    stack = []
-    for line in instream:
+    @classmethod
+    def quote(cls, obj):
         try:
-            command = commandparser(''.join(stack + [line]))
-            stack.clear()
+            return cls.pattern.sub(lambda m: "$lit(%s)" % m.group(), obj)
+        except TypeError:
+            return obj
+
+    def __init__(self, outstream, interactive = False):
+        self.context = Context()
+        self.context['stdout'] = Stream(outstream)
+        self.stack = []
+        self.interactive = interactive
+
+    def __enter__(self):
+        return self
+
+    def printf(self, template, *args):
+        self(template % tuple(self.quote(a) for a in args))
+
+    def __call__(self, line):
+        try:
+            command = commandparser(''.join(self.stack + [line]))
+            self.stack.clear()
         except pyparsing.ParseException:
-            stack.append(line)
-            continue
+            self.stack.append(line)
+            return
         try:
-            execute(command, context)
+            execute(command, self.context)
         except (UnsupportedEntryException, NoSuchPathException, FileNotFoundError):
-            if not interactive:
+            if not self.interactive:
                 raise
             traceback.print_exc(0)
-    if stack:
-        raise DanglingStackException(stack)
+
+    def __exit__(self, *args):
+        if self.stack:
+            raise DanglingStackException(self.stack)
 
 def main():
-    repl(sys.stdin, sys.stdout, True)
+    with Repl(sys.stdout, True) as repl:
+        for line in sys.stdin:
+            repl(line)
 
 if '__main__' == __name__:
     main()
