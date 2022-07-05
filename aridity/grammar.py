101cc91001985addc178ac4230bd9787d0b9f362
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2022-07-05 17:54:19 +0100

    ren

--- aridity/grammar.py
+++ aridity/grammar.py
@@ -56,10 +56,10 @@ class Parser:
             result, = result
         return result
 
-def _innercallpa(s, l, t):
+def _principalcallpa(s, l, t):
     return Call(t[0], t[2:-1], t[1] + t[-1])
 
-def _outercallpa(s, l, t):
+def _additionalcallpa(s, l, t):
     return Call(t[0], t[1:], ['', ''])
 
 class GFactory:
@@ -87,8 +87,8 @@ class GFactory:
             for o, c in self.bracketpairs:
                 yield (Suppress(Regex("[$](?:lit|')")) + Suppress(o) + Regex("[^%s]*" % re.escape(c)) + Suppress(c)).setParseAction(Text.pa)
                 yield (Suppress(Regex('[$](?:pass|[.])')) + getbrackets(Text.pa, Text.pa)).setParseAction(self._bracketspa)
-                yield (Suppress('$') + self.identifier + getbrackets(Blank.pa, AnyScalar.pa)).setParseAction(_innercallpa)
-                yield (Suppress('$') + self.identifier + action).setParseAction(_outercallpa)
+                yield (Suppress('$') + self.identifier + getbrackets(Blank.pa, AnyScalar.pa)).setParseAction(_principalcallpa)
+                yield (Suppress('$') + self.identifier + action).setParseAction(_additionalcallpa)
         optblank = _getoptblank(Blank.pa, self.boundarychars)
         action = Forward()
         action << MatchFirst(clauses()).leaveWhitespace()
